1b9c6b558d60459473fbf8dc976d77b8f2dad255
yoon <parkyoon97@gmail.com>
2024-06-26 15:05:53 +0900

    crm: init

--- src/components/function/carousel.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-let a;
-let b;
-function add(a, b) {
-    return a + b;
-}
-add(1, 2);
--- src/components/function/carousel.ts
+++
@@ -1,8 +0,0 @@
-let a: number;
-let b: number;
-
-function add(a: number, b: number) {
-  return a + b;
-}
-
-add(1, 2);
